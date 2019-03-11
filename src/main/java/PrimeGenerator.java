1e463403b5c31101b9f1247045bd672533b09a1e
Dylan Barrow <barrowdylan9@gmail.com>
2019-03-11 19:36:25 -0400

    Refactored initial checks in isPrime to be done in a single line.

--- src/main/java/PrimeGenerator.java
+++ src/main/java/PrimeGenerator.java
@@ -21,8 +21,8 @@ public class PrimeGenerator implements PrimeNumberGenerator {
     }
 
     public boolean isPrime(int value) {
-        if(value < 2) return false; // By mathematical definition, all values in the range (- infinity, 1] are non prime.
-        if(value > 2 && value % 2 == 0) return false; // any even number larger than 2 is non prime.
+        // By mathematical definition, all values in the range (- infinity, 1] are non prime, as well as all even numbers greater than 2
+        if(value < 2 || (value > 2 && value % 2 == 0)) return false;
         for(int i = 2; i < value; i++) {
             if(value % i == 0) {
                 return false;
