2f6cb1768ec14cc8ca0b7ac8401617ec12887dbd
Dylan Barrow <barrowdylan9@gmail.com>
2019-03-11 18:31:13 -0400

    initial project setup

---
+++ src/main/java/PrimeGenerator.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class PrimeGenerator implements PrimeNumberGenerator {
+
+    public List<Integer> generate(int startingValue, int endingValue) {
+        return null;
+    }
+
+    public boolean isPrime(int value) {
+        return false;
+    }
+}
---
+++ src/main/java/PrimeNumberGenerator.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public interface PrimeNumberGenerator {
+    List<Integer> generate(int startingValue, int endingValue);
+    boolean isPrime(int value);
+}
+
