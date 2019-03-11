fdc1b1e6ccbe2a0778cab098a804311b58a67768
Dylan Barrow <barrowdylan9@gmail.com>
2019-03-11 19:34:38 -0400

    Improved performance by adding initial even number check in isPrime(). All tests passing

--- src/main/java/PrimeGenerator.java
+++ src/main/java/PrimeGenerator.java
@@ -22,6 +22,7 @@ public class PrimeGenerator implements PrimeNumberGenerator {
 
     public boolean isPrime(int value) {
         if(value < 2) return false; // By mathematical definition, all values in the range (- infinity, 1] are non prime.
+        if(value > 2 && value % 2 == 0) return false; // any even number larger than 2 is non prime.
         for(int i = 2; i < value; i++) {
             if(value % i == 0) {
                 return false;
