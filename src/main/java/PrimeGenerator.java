ba10cfe8bdb4d1e044e95d9d04da593dcfcd4d4d
Dylan Barrow <barrowdylan9@gmail.com>
2019-03-11 20:08:47 -0400

    Refactored isPrime to only check up to half of the num being checked. All tests passing

--- src/main/java/PrimeGenerator.java
+++ src/main/java/PrimeGenerator.java
@@ -4,7 +4,6 @@ import java.util.List;
 public class PrimeGenerator implements PrimeNumberGenerator {
 
     public List<Integer> generate(int startingValue, int endingValue) {
-
         if(startingValue > endingValue) {
             int temp = endingValue;
             endingValue = startingValue;
@@ -23,7 +22,7 @@ public class PrimeGenerator implements PrimeNumberGenerator {
     public boolean isPrime(int value) {
         // By mathematical definition, all values in the range (- infinity, 1] are non prime, as well as all even numbers greater than 2
         if(value < 2 || (value > 2 && value % 2 == 0)) return false;
-        for(int i = 3; i < value; i += 2) {
+        for(int i = 3; i < value / 2; i += 2) { // Only need to check up to half of the integers as any int larger than half cannot be a factor as the other factor would be < 2
             if(value % i == 0) {
                 return false;
             }
